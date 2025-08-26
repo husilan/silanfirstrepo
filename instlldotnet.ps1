cd2e2da80a197f4fe9b361b29e57462635cd3307
husilan <372282603@qq.com>
2025-08-26 01:17:24 +0000

    add file

---
+++ instlldotnet.ps1
@@ -0,0 +1,28 @@
+$temp_dir = "C:\Temp"
+if (-not (Test-Path $temp_dir)) { New-Item -ItemType Directory -Path $temp_dir | Out-Null }
+Write-Host "Download dotnet/aspnet runtime installer"
+$runtimeVersion = "8.0.17"
+Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/Runtime/$runtimeVersion/dotnet-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\dotnet-runtime-win-x64.exe"
+Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$runtimeVersion/aspnetcore-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\aspnet-runtime-win-x64.exe"
+
+Write-Host "Instal dotnet runtime $runtimeVersion"
+$dotnet = Start-Process "$temp_dir\dotnet-runtime-win-x64.exe" -Wait -ArgumentList "/install", "/quiet", "/norestart", "/log", "$temp_dir\dotnet.log" -PassThru
+
+if ($dotnet.ExitCode -ne 0) {
+    Write-Host "Failed to install dotnet 8 runtime $runtimeVersion"
+    exit 1
+}
+Write-Host "dotnet runtime $runtimeVersion installed successfully"
+
+Write-Host "Instal asp.net runtime $runtimeVersion"
+$aspnet = Start-Process "$temp_dir\aspnet-runtime-win-x64.exe" -Wait -ArgumentList "/install", "/quiet", "/norestart", "/log", "$temp_dir\aspnet.log" -PassThru
+
+
+if ($aspnet.ExitCode -ne 0) {
+    Write-Host "Failed to install asp.net runtime $runtimeVersion"
+    exit 1
+}
+Write-Host "asp.net runtime $runtimeVersion installed successfully"
+
+Write-Host "List dotnet versions"
+dotnet --list-runtimes
