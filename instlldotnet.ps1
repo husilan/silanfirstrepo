54de47a7937814c387201a378ccb174d76b9501d
husilan <372282603@qq.com>
2025-08-26 10:24:06 -0700

    Update instlldotnet.ps1

--- instlldotnet.ps1
+++ instlldotnet.ps1
@@ -2,9 +2,9 @@ $temp_dir = "C:\Temp"
 if (-not (Test-Path $temp_dir)) { New-Item -ItemType Directory -Path $temp_dir | Out-Null }
 Write-Host "Download dotnet/aspnet runtime installer"
 $runtimeVersion = "8.0.19"
-Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/Runtime/$runtimeVersion/dotnet-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\dotnet-runtime-win-x64.exe"
-Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$runtimeVersion/aspnetcore-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\aspnet-runtime-win-x64.exe"
-Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/$runtimeVersion/windowsdesktop-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\windowsdesktop-runtime-win-x64.exe"
+Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/Runtime/$runtimeVersion/dotnet-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\dotnet-runtime-win-x64.exe" -Force
+Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$runtimeVersion/aspnetcore-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\aspnet-runtime-win-x64.exe" -Force
+Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/$runtimeVersion/windowsdesktop-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\windowsdesktop-runtime-win-x64.exe" -Force
 
 
 Write-Host "Instal dotnet runtime $runtimeVersion"
@@ -27,7 +27,7 @@ if ($aspnet.ExitCode -ne 0) {
 Write-Host "asp.net runtime $runtimeVersion installed successfully"
 
 Write-Host "Instal WindowsDesktop runtime $runtimeVersion"
-$windowsDesktop = Start-Process "$temp_dir\WindowsDesktop-runtime-win-x64.exe" -Wait -ArgumentList "/install", "/quiet", "/norestart", "/log", "$temp_dir\aspnet.log" -PassThru
+$windowsDesktop = Start-Process "$temp_dir\windowsdesktop-runtime-win-x64" -Wait -ArgumentList "/install", "/quiet", "/norestart", "/log", "$temp_dir\aspnet.log" -PassThru
 
 
 if ($windowsDesktop.ExitCode -ne 0) {
