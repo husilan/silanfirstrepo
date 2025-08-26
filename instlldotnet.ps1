7691d59107418746bf869e4b12ef1644a71efcac
husilan <372282603@qq.com>
2025-08-26 10:17:07 -0700

    Update .NET runtime version to 8.0.19

--- instlldotnet.ps1
+++ instlldotnet.ps1
@@ -1,9 +1,11 @@
 $temp_dir = "C:\Temp"
 if (-not (Test-Path $temp_dir)) { New-Item -ItemType Directory -Path $temp_dir | Out-Null }
 Write-Host "Download dotnet/aspnet runtime installer"
-$runtimeVersion = "8.0.17"
+$runtimeVersion = "8.0.19"
 Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/Runtime/$runtimeVersion/dotnet-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\dotnet-runtime-win-x64.exe"
 Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$runtimeVersion/aspnetcore-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\aspnet-runtime-win-x64.exe"
+Invoke-webrequest -uri https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/$runtimeVersion/windowsdesktop-runtime-$runtimeVersion-win-x64.exe -outfile "$temp_dir\windowsdesktop-runtime-win-x64.exe"
+
 
 Write-Host "Instal dotnet runtime $runtimeVersion"
 $dotnet = Start-Process "$temp_dir\dotnet-runtime-win-x64.exe" -Wait -ArgumentList "/install", "/quiet", "/norestart", "/log", "$temp_dir\dotnet.log" -PassThru
@@ -23,6 +25,16 @@ if ($aspnet.ExitCode -ne 0) {
     exit 1
 }
 Write-Host "asp.net runtime $runtimeVersion installed successfully"
+
+Write-Host "Instal WindowsDesktop runtime $runtimeVersion"
+$windowsDesktop = Start-Process "$temp_dir\WindowsDesktop-runtime-win-x64.exe" -Wait -ArgumentList "/install", "/quiet", "/norestart", "/log", "$temp_dir\aspnet.log" -PassThru
+
+
+if ($windowsDesktop.ExitCode -ne 0) {
+    Write-Host "Failed to install WindowsDesktop runtime $runtimeVersion"
+    exit 1
+}
+Write-Host "WindowsDesktop runtime $runtimeVersion installed successfully"
 
 Write-Host "List dotnet versions"
 dotnet --list-runtimes
